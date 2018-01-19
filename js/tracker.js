25e500dd9af91b3773f5c487731cf75e1391b452
Wendy <zhaod.wendy@gmail.com>
2018-01-18 22:38:19 -0800

    added js tracking script

---
+++ js/tracker.js
@@ -0,0 +1,64 @@
+	//Searches for necessary input field, and if existing, executes script to embed tracking ID
+	//Returns false if non-existent cid
+
+	function exists(element) {
+	  return (element !== null)
+	}
+
+	function getClientId(uaid) {
+	    try {
+	        var trackers = ga.getAll();
+	        var i, len;
+	        for (i = 0, len = trackers.length; i < len; i += 1) {
+	            if (trackers[i].get('trackingId') === uaid) {
+	                return trackers[i].get('clientId');
+	            }
+	        }
+	    } catch (e) {
+	        console.log("Error fetching clientId");
+	    }
+	    return 'false';
+	}
+
+	function insertCid(){
+		//Check for labelName Existence
+		let labelNames = document.querySelectorAll(".zf-labelName");
+		//console.log("Here are the labels: ");
+		//console.log(labelNames);
+		if (labelNames.length > 0){
+			var trackingField = false
+			labelNames.forEach(field =>{
+			    if (field.innerText.toLowerCase().trim()!=="tracking id"){
+			        console.log("Not the tracking ID field");
+					return;
+			    } else {
+                   	//Attach CID to the correct input field
+                  	field.nextElementSibling.querySelector("input").id="cid";
+					trackingField = true;
+			    }
+			})
+		} else {
+			console.log("No form input fields on page");
+		}
+      	return trackingField == true;
+	}
+
+	window.onload = function() {
+			if (insertCid()){
+				let cidInput = document.querySelector("#cid");
+				console.log("The tracking Input field: ")
+		  		console.log(cidInput);
+	  			//If tracking field exists, attach tracking id to value	
+				if (exists(cidInput)) {
+				  cidInput.value = getClientId('UA-71237243-1');
+				  // cidInput.value = 'Inputting an ID here to test';
+				} else {
+				  console.log("No input element for cid");
+				  return false
+				}
+			} else {
+				console.log("insert CID function failed");
+				return false;
+			}
+			
+	}
